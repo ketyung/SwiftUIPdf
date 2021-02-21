a4ba7ff0728ca04352a76ca9dfbc14440f488c61
techchee <ketyung@techchee.com>
2021-02-21 09:05:07 +0800

    Initial Commit

---
+++ SwiftUIPdf/SwiftUIPdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPdfApp.swift
+//  SwiftUIPdf
+//
+//  Created by Chee Ket Yung on 21/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
